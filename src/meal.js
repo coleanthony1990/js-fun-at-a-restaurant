4b9afa32b12d0d0033488f3c25482db01cbcf1f5
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-07-10 17:22:54 -0600

    Add Meal and Order

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,46 @@
-function nameMenuItem() {
+function nameMenuItem(item1) {
+  return `Delicious ${item1}`
+}
 
+function createMenuItem(name, price, type) {
+  var item = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return item;
+}
+
+function addIngredients(topping, ingredients) {
+  
+  if (!ingredients.includes(topping)) {
+    ingredients.push(topping);
+    return ingredients
+  }
+}
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+function decreasePrice(oldPrice) {
+  return oldPrice *= 1 - 0.10
+}
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+  return recipe
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
+function takeOrder(newOrder, deliveryOrders) {
+  if (deliveryOrders.length < 3)
+    deliveryOrders.push(newOrder)
+    return deliveryOrders
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNumber)
+    {
+      return deliveryOrders.splice(i, 1)
+    }
+}
+}
+function listItems(deliveryOrders) {
+  var items = []
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    items.push(deliveryOrders[i].item)
+  }
+  return items.join(', ')
+}
+
+function searchOrder(deliveryOrders, name) {
+  var order = false
+  for (var i = 0; i < deliveryOrders.length; i++) {
+  if (deliveryOrders[i].item === name) {
+    order = true
+  }
+}
+  return order
+}
+
+
+
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
